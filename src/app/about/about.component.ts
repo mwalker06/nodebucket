959f37e5b7161b74702113f3d5247f99619147c2
mwalker06 <81312304+mwalker06@users.noreply.github.com>
2023-09-02 00:11:51 -0500

    added page comments and attribution

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,3 +1,9 @@
+/* Title: about.component
+Author: Megan Walker
+Date: 09-02-2023
+Description: about.component.ts
+Source: Professor Krasso, Angular.io */
+
 import { Component } from '@angular/core';
 
 @Component({
--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,3 +1,9 @@
+/* Title: not-found.component
+Author: Megan Walker
+Date: 09-02-2023
+Description: not-found.component.ts
+Source: Professor Krasso, Angular.io */
+
 import { Component } from '@angular/core';
 
 @Component({
